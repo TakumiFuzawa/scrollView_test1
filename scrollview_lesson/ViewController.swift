3e094f5fb20ebd673aa5aa65dbc98e63208a7169
github TakumiFuzawa <futaku0811@gmail.com>
2020-02-16 13:35:30 +0900

    Initial Commit

---
+++ scrollview_lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrollview_lesson
+//
+//  Created by Takumi Fuzawa on 2020/02/16.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ scrollview_lessonTests/scrollview_lessonTests.swift
@@ -0,0 +1,34 @@
+//
+//  scrollview_lessonTests.swift
+//  scrollview_lessonTests
+//
+//  Created by Takumi Fuzawa on 2020/02/16.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import XCTest
+@testable import scrollview_lesson
+
+class scrollview_lessonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
